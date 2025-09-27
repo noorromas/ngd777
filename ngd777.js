a7defe248c5c20a2853b0e9e2986461e22f8b3f6
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-09-27 10:26:22 +0300

    Update ngd777.js

--- ngd777.js
+++ ngd777.js
@@ -1,1 +1,81 @@
-﻿$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"><u><b>اشتراكات شات نجد - اضغط هنا</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" ><p style="text-align: center"><b><font color="#000080">اشتراكات الشات</font></b></p></p><div style="float: right;padding-right: 20px;">- 1</div> شترك ادمن ذهبي 650 ريال لمدة شهرين ) <img src="https://www.njd.chat/sico/zvCFi1N2kj.gif"></p><div style="float: right;padding-right: 20px;">- 2</div> اشترك ادمن اسود 350 ريال لمدة شهرين <img src="https://www.njd.chat/sico/zvqhLTljtX.gif"></p><div style="float: right;padding-right: 20px;">- 3</div> اشترك تميز نجد  200 ريال لمدة شهرين <img src="https://www.njd.chat/sico/zsjUnTM2vK.gif"> </p><div style="float: right;padding-right: 20px;"></div><img src=""><p><b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة شهرين</font></b><p style="text-align: center"><u><b><a target="_blank" href="https://www.njd.chat/esh.html">وللتواصل مع الاداره اضغط هنا</a></b></u></font></div> </div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$(`<center>
+  <div style="position: static; width: 100%;" onclick="setTimeout(function(){fixSize();},800);">
+    <div id="design-a"
+         width="99.5%"
+         style="background-color: antiquewhite;
+                color: #105873;
+                border-bottom: 3px solid #105873;
+                border-top: 3px solid #6b5f5f;
+                padding-bottom: 2px;
+                border-radius: 0px 0px 15px 15px;">
+
+      <font id="sultan" class="sultan11">
+        <u><b>اشتراكات نجد - ضغط هنا</b></u>
+      </font>
+
+      <div id="spanan"
+           style="font-family: jazeera-light, FontAwesome;
+                  color: red;
+                  text-align: right;
+                  display: none;">
+
+        <p style="text-align: center">
+          <b><font color="#000080">اشتراكات الشات</font></b>
+        </p>
+
+        <p>
+          <div style="float: right; padding-right: 20px;">- 1</div>
+           اشترك ادمن ذهبي شهرين 650ريال واحصل على (2 ادمن اسود )
+          <img src="https://www.njd.chat/sico/zvCFi1N2kj.gif">
+        </p>
+
+        <p>
+          <div style="float: right; padding-right: 20px;">- 2</div>
+          اشترك ادمن اسود شهرين 350ريال واحصل على ( تاج ذهبي )
+          <img src="https://www.njd.chat/sico/zvqhLTljtX.gif">
+        </p>
+
+        <p>
+          <div style="float: right; padding-right: 20px;">- 3</div>
+          تاج تميز نجد (المخفي) بـ 200ريال لمدة : شهرين
+          <img src="https://up6.cc/2025/08/175895766567491.gif">
+        </p>
+
+        <p>
+          <div style="float: right; padding-right: 20px;">- 4</div>
+          تاج فضي بـ 200ريال لمدة : شهرين
+          <img src="https://up6.cc/2025/08/175895772189571.gif">
+        </p>
+
+        <p>
+          <div style="float: right; padding-right: 20px;">- 5</div>
+          نجمتين ذهبي بـ 150 ريال لمدة : شهر
+          <img src="https://up6.cc/2025/08/175895779050061.gif">
+        </p>
+
+        <p>
+          <div style="float: right; padding-right: 20px;">- 6</div>
+          ثلاث نجوم سوداء بـ 100 ريال لمدة : شهر
+          <img src="https://up6.cc/2025/08/175895774663421.gif">
+        </p>
+
+        <p>
+          <b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة شهر</b>
+        </p>
+
+        <p style="text-align: center">
+          <u><b>
+            <a target="_blank" href="https://www.njd.chat/esh.html">
+              وللتواصل مع الاداره اضغط هنا
+            </a>
+          </b></u>
+        </p>
+
+      </div>
+    </div>
+  </div>
+</center>`).insertBefore('#d2');
+
+$("#design-a").click(function(){
+  $("#spanan").slideToggle(function(){});
+});
